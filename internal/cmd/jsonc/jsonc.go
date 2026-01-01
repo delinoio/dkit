4faf2c563ee4cdd8c8fe29572dbae3733bddb153
DongYun Kang <kdy.1997.dev@gmail.com>
2026-01-01 12:30:30 +0900

    json

--- internal/cmd/jsonc/jsonc.go
+++ internal/cmd/jsonc/jsonc.go
@@ -1,7 +1,13 @@
 package jsonc
 
 import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/delinoio/dkit/internal/utils"
 	"github.com/spf13/cobra"
+	"github.com/tailscale/hujson"
 )
 
 func NewCommand() *cobra.Command {
@@ -30,7 +36,50 @@ func newCompileCommand() *cobra.Command {
 If no file is specified, reads from stdin.`,
 		Args: cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: Implement jsonc compile
+			var input io.Reader
+
+			if len(args) > 0 {
+				// Read from file
+				file, err := os.Open(args[0])
+				if err != nil {
+					utils.PrintError("File not found: %s", args[0])
+					return err
+				}
+				defer file.Close()
+				input = file
+			} else {
+				// Read from stdin
+				input = os.Stdin
+			}
+
+			// Read all input
+			data, err := io.ReadAll(input)
+			if err != nil {
+				utils.PrintError("Failed to read input: %v", err)
+				return err
+			}
+
+			// Parse JSONC
+			ast, err := hujson.Parse(data)
+			if err != nil {
+				utils.PrintError("Invalid JSONC syntax: %v", err)
+				os.Exit(1)
+			}
+
+			// Standardize to JSON
+			ast.Standardize()
+
+			// Output
+			output := ast.Pack()
+			if pretty {
+				// Pretty print
+				formatted, err := hujson.Format(output)
+				if err == nil {
+					output = formatted
+				}
+			}
+
+			fmt.Fprintf(os.Stdout, "%s\n", output)
 			return nil
 		},
 	}
